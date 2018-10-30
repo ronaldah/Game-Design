8903d9f9de2c6a0073df8f73cb591ae5557ddbe5
christopher1027 <peytonorourke@gmail.com>
2018-10-30 19:10:52 -0400

    UI scripts

---
+++ DungeonCrawl/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour {
+
+    private HealthSystem healthSystem;
+
+    [SerializeField]
+    private float fillAmount;
+
+    [SerializeField]
+    private Image content;
+
+    public void Setup(HealthSystem healthSystem)
+    {
+        this.healthSystem = healthSystem;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        HandleBar();
+    }
+    private void HandleBar()
+    {
+        //Debug.Log("Health Percent: " + healthSystem.GetHealthPercent());
+        content.fillAmount = healthSystem.GetHealthPercent();
+        //Debug.Log("Fill Amount: " + content.fillAmount);
+    }
+}
+
+/*
+public void Setup(HealthSystem healthSystem)
+{
+    this.healthSystem = healthSystem;
+
+    healthSystem.OnHealthChanged += HealthSystem_OnHealthChanged;
+}
+
+private void HealthSystem_OnHealthChanged(object sender, System.EventArgs e)
+{
+    throw new System.NotImplementedException();
+}
+*/
