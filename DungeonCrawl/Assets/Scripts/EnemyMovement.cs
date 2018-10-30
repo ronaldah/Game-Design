56afd7086ad49003da54d63e047dab34d1aad7ab
Alexander Reeser <areeser42@gmail.com>
2018-10-30 18:37:20 -0400

    Enemy now has animation.

--- DungeonCrawl/Assets/Scripts/EnemyMovement.cs
+++ DungeonCrawl/Assets/Scripts/EnemyMovement.cs
@@ -9,43 +9,26 @@ public class EnemyMovement : MonoBehaviour {
     public float force = 100f;
     public float maxSpeed = 8.0f;
     public float closeEnoughToHome = 2;
-<<<<<<< HEAD
-    bool goHome = false;
     public Vector3 moveVect;
     public Vector3 startPosition;
     public Rigidbody rigid;
-
-	// Use this for initialization
-	void Start () {
-        rigid = GetComponent<Rigidbody>();
-        Player = GameObject.FindWithTag("Player");
-        startPosition = gameObject.transform.position;
-	}
-
-    // Update is called once per frame
-    void Update()
-=======
     public float closeEnoughToPlayer = 5;
     public float patrolTime = 3;
     bool goHome = false;
     bool isHome = true;
-    public Vector3 moveVect;
-    public Vector3 startPosition;
-    public Rigidbody rigid;
     public Animation anime;
     float velMagnitude;
 
   // Use this for initialization
- void Start () {
+    void Start () {
         rigid = GetComponent<Rigidbody>();
         Player = GameObject.FindWithTag("Player");
         startPosition = gameObject.transform.position;
         anime = GetComponent<Animation>();
-   }
+    }
 
     // Update is called once per frame
     void FixedUpdate()
->>>>>>> 6152d0ca0243e0a284f9949372f5420418907c74
     {
         if (goHome)
         {
@@ -53,19 +36,13 @@ public class EnemyMovement : MonoBehaviour {
             if (moveVect.magnitude <= closeEnoughToHome)
             {
                 goHome = false;
-<<<<<<< HEAD
-=======
                 isHome = true;
->>>>>>> 6152d0ca0243e0a284f9949372f5420418907c74
                 return;
             }
             moveVect *= Time.deltaTime;
             moveVect /= moveVect.magnitude;
-<<<<<<< HEAD
             float velMagnitude = rigid.velocity.magnitude;
-=======
             velMagnitude = rigid.velocity.magnitude;
->>>>>>> 6152d0ca0243e0a284f9949372f5420418907c74
             if (velMagnitude > maxSpeed)
             {
                 rigid.velocity = rigid.velocity / velMagnitude * maxSpeed;
@@ -78,14 +55,11 @@ public class EnemyMovement : MonoBehaviour {
     {
         if (other.gameObject.tag == "Player")
         {
-<<<<<<< HEAD
             moveVect = Player.transform.position - gameObject.transform.position;
-=======
             isHome = false;
             moveVect = Player.transform.position - gameObject.transform.position;
             float x = moveVect.magnitude;
             if (x < closeEnoughToPlayer)
->>>>>>> 6152d0ca0243e0a284f9949372f5420418907c74
             moveVect = moveVect * Time.deltaTime;
             moveVect /= moveVect.magnitude;
             float magnitude = rigid.velocity.magnitude;
@@ -94,13 +68,10 @@ public class EnemyMovement : MonoBehaviour {
                 rigid.velocity = rigid.velocity / magnitude * maxSpeed;
             }
             rigid.AddRelativeForce(moveVect * force);
-<<<<<<< HEAD
-=======
             if (x < closeEnoughToPlayer)
             {
                 anime.Play();
             }
->>>>>>> 6152d0ca0243e0a284f9949372f5420418907c74
         }
     }
 
@@ -112,8 +83,4 @@ public class EnemyMovement : MonoBehaviour {
         }
     }
 }
-<<<<<<< HEAD
 
-
-=======
->>>>>>> 6152d0ca0243e0a284f9949372f5420418907c74
