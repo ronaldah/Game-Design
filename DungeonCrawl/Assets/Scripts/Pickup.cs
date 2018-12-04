fadb01f6e3e364549064c875270f07f1a9a80d10
Alexander Reeser <areeser42@gmail.com>
2018-12-04 16:00:51 -0500

    Fixed some wonky physics.

--- DungeonCrawl/Assets/Scripts/Pickup.cs
+++ DungeonCrawl/Assets/Scripts/Pickup.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using UnityEngine;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
@@ -30,7 +29,7 @@ public class Pickup : MonoBehaviour {
                     item = hit.transform.gameObject.GetComponent<Item>();
 
                     //Pick up item
-                    Inventory.AddItem(item);
+                    //Inventory.AddItem(item);
                     Debug.Log("Picking up " + item.itemName);
 
                     Destroy(hit.transform.gameObject);
@@ -71,137 +70,3 @@ public class Pickup : MonoBehaviour {
         }
     }
 }
-
-/*
-
-
-            if (hit.transform.gameObject.tag == "Weapon")
-            {
-                if (Input.GetMouseButtonDown(0))
-                {
-                    Player.player.GetComponent<Player>().PickupWeapon();
-                    Destroy(hit.transform.gameObject);
-                }
-                CenterPoint.action = "Pick Up";
-            }
-            else if (hit.transform.gameObject.tag == "Shield")
-            {
-                if (Input.GetMouseButtonDown(0))
-                {
-                    Player.player.GetComponent<Player>().PickupShield();
-                    Destroy(hit.transform.gameObject);
-                }
-                CenterPoint.action = "Pick Up";
-            }
-            else
-            {
-                if (Input.GetMouseButtonDown(0) && !Player.defending)
-                    Player.attacking = true;
-                CenterPoint.action = null;
-            }
-        }
-        else {
-            if (Input.GetMouseButtonDown(0) && !Player.defending)
-                Player.attacking = true;
-            CenterPoint.action = null;
-        }
-    }
-}
-=======
-﻿using UnityEngine;
-using System.Collections.Generic;
-using UnityEngine.SceneManagement;
-using System.Collections;
-
-public class Pickup : MonoBehaviour {
-
-    public Camera mainCamera;
-    public int rayDistance = 5;
-    Item item;
-    Player player;
-
-    // Use this for initialization
-    void Start() {
-        mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
-    }
-
-    // Update is called once per frame
-    void Update() {
-        RaycastHit hit;
-        
-        if (Physics.Raycast(mainCamera.transform.position, mainCamera.transform.forward, out hit, rayDistance))
-        {
-            if (hit.transform.gameObject.tag == "Item")
-            {
-                CenterPoint.action = "Pick Up";
-
-                //Pick up Item
-                if (Input.GetMouseButtonDown(0))
-                {
-          
-                    //Get raycasted gameObject to an Item object
-                    item = hit.transform.gameObject.GetComponent<Item>();
-
-                    //Pick up item
-                    Debug.Log(Player.inventory);
-                    Player.inventory.AddItem(item);
-
-
-                    Debug.Log("Picking up " + item.itemName);
-
-                    Destroy(hit.transform.gameObject);
-                }
-            }    
-        }
-    }
-}
-
-/*
- //Drop Item
-                if (Input.GetMouseButtonDown(1))
-                {
-                    //Get raycasted gameObject to an Item object
-                    item = hit.transform.gameObject.GetComponent<Item>();
-
-                    Debug.Log("Dropping: " + item.itemName);
-                    Player.inventory.RemoveItem(item);
-                }
-*/
-
-/*
-
-
-            if (hit.transform.gameObject.tag == "Weapon")
-            {
-                if (Input.GetMouseButtonDown(0))
-                {
-                    Player.player.GetComponent<Player>().PickupWeapon();
-                    Destroy(hit.transform.gameObject);
-                }
-                CenterPoint.action = "Pick Up";
-            }
-            else if (hit.transform.gameObject.tag == "Shield")
-            {
-                if (Input.GetMouseButtonDown(0))
-                {
-                    Player.player.GetComponent<Player>().PickupShield();
-                    Destroy(hit.transform.gameObject);
-                }
-                CenterPoint.action = "Pick Up";
-            }
-            else
-            {
-                if (Input.GetMouseButtonDown(0) && !Player.defending)
-                    Player.attacking = true;
-                CenterPoint.action = null;
-            }
-        }
-        else {
-            if (Input.GetMouseButtonDown(0) && !Player.defending)
-                Player.attacking = true;
-            CenterPoint.action = null;
-        }
-    }
-}
->>>>>>> c39d2380ccba1a6441c137c5b6636bf2d16cfa19
-*/
